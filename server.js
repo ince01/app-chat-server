c8727988c39a6030629befb6e3226f4ab7f1ae55
ince <alt.f4.to@gmail.com>
2019-01-04 12:38:40 +0700

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
