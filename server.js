c0ac36b4d5a0d710042673660bc5491d99e19c42
ince <alt.f4.to@gmail.com>
2019-01-04 11:55:43 +0700

    make it better

---
+++ server.js
@@ -0,0 +1,78 @@
+var app = require('express');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var User = require('./models/users');
+var Message = require('./models/message');
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyParser.json())
+
+var dbUri = 'mongodb://admin:admin123@ds231205.mlab.com:31205/app-chat'
+
+const options = {
+    useNewUrlParser: true,
+}
+
+mongoose.connect(dbUri, options).then(
+    () => {
+        console.log('Database is connected')
+    },
+    (err) => {
+        console.log(`Can not connect to the database: ${err}`)
+    })
+
+mongoose.Promise = global.Promise
+
+var db = mongoose.connection
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', function () {
+//     console.log('Connected database !')
+// });
+
+app.get('/', function (req, res) {
+    res.json('Hello world !');
+})
+
+app.post('/user', function (req, res) {
+    const user = new User(req.body);
+    user.save()
+        .then(function (result) {
+            res.status(200).json(result);
+        })
+        .catch(function (err) {
+            res.status(400).send('unable to save the course into database');
+            console.log(err);
+        })
+})
+
+io.on('connection', function (socket) {
+    console.log(`a user connected with id: ${socket.id}`);
+    socket.on('disconnect', function () {
+        console.log(`a user disconnected with id: ${socket.id}`);
+    });
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+        const message = new Message({"message":msg});
+        message.save()
+            .try(function (result) {
+                console.log(result)
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    });
+})
+
+server.listen(PORT, function () {
+    console.log(`Server is running on port ${PORT} ......`)
+})
